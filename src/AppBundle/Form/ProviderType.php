6e4787a39c57c4f84b4e59df3e900e2c7b782999
BAYE Gilles <bayeg@isl.be>
2017-12-04 21:23:13 +0100

    20171204_form_start

---
+++ src/AppBundle/Form/ProviderType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProviderType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')
+            ->add('website')
+            ->add('contactEmail')
+            ->add('phone')
+            ->add('vat')
+//            ->add('slug')
+            ->add('serviceCategories', EntityType::class, [
+                'class' => 'AppBundle\Entity\ServiceCategory',
+                'choice_label' => 'name',
+                'multiple' => true
+            ])
+            ->add('internauts');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Provider'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_provider';
+    }
+
+
+}
