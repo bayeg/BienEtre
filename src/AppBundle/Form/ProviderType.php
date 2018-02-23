58dc613900519eab8e5cc36950b768e10788a1e8
bayeg <gilles.baye@hotmail.com>
2018-02-23 17:07:11 +0100

    20180223_authentiator

---
+++ src/AppBundle/Form/ProviderType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProviderType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('website')
+            ->add('contactEmail')
+            ->add('phone')
+            ->add('vat')
+            ->add('slug')
+            ->add('serviceCategories')
+            ->add('internauts');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Provider'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_provider';
+    }
+
+
+}
